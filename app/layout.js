56e39694fb9028339a1f5c411b0a092ef6bc765a
Maintenance <maintenance@Maintenence-PC.localdomain>
2024-08-14 13:11:27 -0700

    add dark mode

--- app/layout.js
+++ app/layout.js
@@ -11,9 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} style={{background: "#292929"}}>
         <AppRouterCacheProvider>
-           {children}
+          {children}
         </AppRouterCacheProvider>
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,7 +1,26 @@
-import Button from '@mui/material/Button';
+"use client";
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { TextField, Button, Box } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark', // Enable dark mode
+  },
+});
 
 export default function Home() {
   return (
-    <Button variant="contained">Hello world</Button>
+    <ThemeProvider theme={theme}>
+      <Box display="flex" alignItems="center" sx={{ p: 2, background: "#121212", borderRadius: 2 }}>
+        <TextField 
+          fullWidth
+          label="Enter Message"
+          sx={{ marginRight: 2 }}
+        />
+        <Button size="large" variant="contained" endIcon={<SendIcon />} sx={{ padding: '14px 24px' }}>Send</Button>
+      </Box>
+    </ThemeProvider>
   );
 }
